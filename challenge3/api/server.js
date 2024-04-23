23d7db798437356e80ff192e3701c5fa0eb53881
ericsul <99050264+ericsul@users.noreply.github.com>
2024-04-22 18:35:22 -0600

    commit to main
    commit the code

--- challenge3/api/server.js
+++ challenge3/api/server.js
@@ -119,7 +119,7 @@ app.get("/api/books/:id", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8088;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
